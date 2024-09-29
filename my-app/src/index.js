e6e84f1105c6b6ec4bd6d9c8047bd2550a1fad50
Aline Cerqueira de Andrade <alinewacerqueira@gmail.com>
2024-09-29 19:53:46 -0300

    refactor(front): aloca padroes de estilo em variaveis

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 const MenuBar = () => {
+  const menu1 = "Gerar Receita"
+  const menu2 = "Meu histórico de receitas"
+  const menu3 = "Todas as receitas"
+
   return (
     <header>
       <nav className="menuBar">
@@ -14,9 +18,9 @@ const MenuBar = () => {
               </a>
             </div>
           </li>
-          <li className="link"><a href="#">Gerar receita</a></li>
-          <li className="link"><a href="#">Meu histórico de receitas</a></li>
-          <li className="link"><a href="#">Todas as receitas</a></li>
+          <li className="link"><a href="#">{menu1}</a></li>
+          <li className="link"><a href="#">{menu2}</a></li>
+          <li className="link"><a href="#">{menu3}</a></li>
         </ul>
       </nav>
     </header>
