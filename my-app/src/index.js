5a81162143ac8ec7a67aed626dabbd4d80f1f909
Aline Cerqueira de Andrade - 822161400 <822161400@ulife.com.br>
2024-09-30 19:19:26 -0300

    refactor(front): inclui botao de resetar receita

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const MenuBar = () => {
   const menu1 = "Gerar Receita"
