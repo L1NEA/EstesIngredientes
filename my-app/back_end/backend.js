fae05467b83116e0fe6ee34deea490eb0248001b
MatheusMC2 <mix.monte@gmail.com>
2024-10-10 10:50:06 -0300

    feat: adiciona rota `hello-world`

--- my-app/back_end/backend.js
+++ my-app/back_end/backend.js
@@ -1,28 +1,14 @@
 // Conexão com a API e EndPoint
 require('dotenv').config()
 const app = require('express')()
-const { OpenAI } = require('openai')
 
 // Função middleware
 app.use(express.json())
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY
-console.log(OPENAI_API_KEY)
-
-//POST /pergunte-ao-chatgpt () => {}
-// ENVIO DE REQUISIÇÃO
-app.post('/pergunte-ao-chatgpt', (req, res) => {
-    const openai = new OpenAI(OPENAI_API_KEY)
-    res.json({mensagem: 'Ok'})
-})
-
-//GET /ingredientes-receita () => {}
 // RECEBIMENTO DE REQUISIÇÃO
-app.get('/oi', (req, res) => {
-    res.json({mensagem: 'oi, $(req.query.nome)'})
+app.get('/hello-world', (req, res) => {
+    res.json({mensagem: 'Olá Mundo!!!, $(req.query.nome)'})
 })
 
 // Escuta ativa de requisições na porta :3000
 app.listen(3000, () => {console.log("Serviço correndo.")})
-
-// Pacote API Chat GPT = npm i openai
