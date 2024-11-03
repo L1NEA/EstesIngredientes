a5d29b993aa99afd646cc5179d5552c248e61b6c
MatheusMC2 <mix.monte@gmail.com>
2024-11-03 13:42:56 -0300

    feat: adiciona registros de excucao na base e endpoint para consulta de logs na base

--- my-app/src/back/backend.js
+++ my-app/src/back/backend.js
@@ -1,14 +1,42 @@
 // Conexão com a API e EndPoint
-require('dotenv').config()
-const app = require('express')()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { connectAndCreateTableLogs, insertLog, selectLogs } = require('./criaTabela');
 
 // Função middleware
-app.use(express.json())
+app.use(express.json());
+
+// Chamar a função para conectar e criar a tabela
+connectAndCreateTableLogs();
+
+// Middleware para registrar logs
+app.use(async (req, res, next) => {
+  res.on('finish', async () => {
+    const endpoint = req.originalUrl;
+    const status = res.statusCode;
+    const erro = res.statusCode >= 400 ? res.statusMessage : null;
+    await insertLog(endpoint, status, erro);
+  });
+  next();
+});
 
 // RECEBIMENTO DE REQUISIÇÃO
 app.get('/hello-world', (req, res) => {
-    res.json({mensagem: `Olá Mundo!!!, ${req.query.nome}`})
-})
+  res.json({ mensagem: `Olá Mundo!!!, ${req.query.nome}` });
+});
+
+// Rota para consultar logs
+app.get('/consultarLogs', async (req, res) => {
+  try {
+    const logs = await selectLogs();
+    res.json(logs);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao consultar logs' });
+  }
+});
 
 // Escuta ativa de requisições na porta :3000
-app.listen(3000, () => {console.log("Serviço correndo na porta localhost:3000")})
+app.listen(3000, () => {
+  console.log("Serviço correndo na porta localhost:3000");
+});
